2ad5f8cbe2bf8cdbf5c2d8d6a6716c1f76e8c66d
Alexandra Dzygal <sdzygal@gmail.com>
2023-01-27 20:13:26 -0700

    Google API for OAuth"

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'there' });
-});
 
 
-app.listen(5000);
+passport.use(
+    new GoogleStrategy(
+        {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback' //the route where user will be sent after he granted the permission
+    },
+        (accessToken, refreshToken, profile, done) => {
+            console.log('access token', accessToken);
+            console.log('refresh token', refreshToken);
+            console.log('profile', profile);
+    })
+);
+
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+    scope: ['profile', 'email']  //access we`re asking for
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
+
