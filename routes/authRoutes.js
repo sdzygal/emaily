a00e4f3c22d7294b72f1244bd1faae111d5ca581
Alexandra Dzygal <sdzygal@gmail.com>
2023-01-31 15:00:07 -0700

    Testing auth and logging out users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,15 @@ module.exports = app => {
     );
 
     app.get('/auth/google/callback', passport.authenticate('google'));
+
+
+    app.get('/api/logout', (req, res) => {
+       req.logout(); //is a function that is automatically attached to the req object by passport. It takes the cookie that contains user id, and it kills the cookie.
+        res.send(req.user); //prove that user no longer signed in.
+    });
+
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    })
 };
