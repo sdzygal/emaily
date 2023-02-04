3a1db4d7abab96e7e71fed8de475a2bb2e819198
Alexandra Dzygal <sdzygal@gmail.com>
2023-02-03 21:32:52 -0700

    New billing route, creating a charge

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import StripeCheckout from 'react-stripe-checkout';
-
+import { connect } from "react-redux";
+import * as actions from '../actions';
 
 
 class Payments extends Component {
@@ -11,7 +12,7 @@ class Payments extends Component {
              name="Emaily"
              description="5$ for 5 email credits"
              amount={500} //money in US 500 cents = 5 dollars
-             token={token => console.log(token)} //callback which works after stripe tells us we got money
+             token={token => this.props.handleToken(token)} //callback which works after stripe tells us we got money
              stripeKey={process.env.REACT_APP_STRIPE_KEY}
          >
              <button className="btn">
@@ -22,4 +23,4 @@ class Payments extends Component {
     }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require("./config/keys");
 require('./models/User'); //first goes always model
 require('./services/passport'); //execute the passport file
@@ -14,6 +15,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
      maxAge: 30 * 24 * 60 * 60 * 1000, //I want this cookie to last 30 days and then expire
@@ -25,6 +27,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app); //return a function
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
