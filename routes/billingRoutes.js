b0dd5c6aed9573870aa8648ca5eed5bc52e0460f
Alexandra Dzygal <sdzygal@gmail.com>
2023-02-04 17:21:04 -0700

    Route-specific middlware of auth

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,22 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')('keys.stripeSecretKey');
+const requireLogin = require('../middlewares/requireLogin');
+
 
 //create charge and send res to our api
 module.exports = app => {
-    app.post('/api/stripe', (req, res) => {
-        stripe.charges.create({
+    app.post('/api/stripe', requireLogin, async (req, res) => {
+      const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for 5 credits',
             source: req.body.id
-        })
+        });
+
+        req.user.credits += 5; //current user assigns by passport
+        const user = await req.user.save();
+
+
+        res.send(user);
     });
 };
