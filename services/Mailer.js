b5c9dcbef9c162c759cede873aad01ebd80e0d4d
Alexandra Dzygal <sdzygal@gmail.com>
2023-02-06 18:26:17 -0700

    Finishing Mailer object

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,7 +3,53 @@ const helper = sendgrid.mail; //helps create a mailer
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super(); //es 2015 classes
 
+        this.sgApi= sendgrid(keys.sendGridKey);
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+
+
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+           return new helper.Email(email);
+        });
+    }
+
+    //sendgrid docs
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, false);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+
+        const response = this.sgApi.API(request);
+        return response;
+    }
 }
 
 module.exports = Mailer;
